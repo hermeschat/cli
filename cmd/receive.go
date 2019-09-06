f2f9ceca2214f33f0fc5990d5c8648059f4eb86a
amirrezaask <raskarpour@gmail.com>
2019-09-06 19:10:26 +0430

    better ux :)

--- cmd/receive.go
+++ cmd/receive.go
@@ -1,44 +1,34 @@
-/*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
 // receiveCmd represents the receive command
 var receiveCmd = &cobra.Command{
-	Use:   "receive",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "receiver",
+	Short: "hermes-cli receiver mode",
+	Long: `in receiver mode you can receive messages
+	usage:
+		hermes-cli receive`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("receive called")
+
+		sigs := make(chan os.Signal)
+		signal.Notify(sigs, syscall.SIGTERM)
+		fmt.Println("Waiting for any message")
+		<-sigs
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(receiveCmd)
 
+	rootCmd.AddCommand(receiveCmd)
+	receiveCmd.Aliases = []string{"recv"}
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
